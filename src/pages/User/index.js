8220e022d8e9c4950b33ce270a5dc81a46463183
vitalves <vitalvesilva@gmail.com>
2019-10-07 16:10:25 -0300

    loading

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
 import api from '../../services/api';
 
 import {
@@ -33,9 +34,13 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    page: 1,
+    loading: false,
   };
 
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const { navigation } = this.props;
     const user = navigation.getParam('user');
     // console.log(user);
@@ -43,13 +48,16 @@ export default class User extends Component {
     const response = await api.get(`/users/${user.login}/starred`);
     // console.log(response);
 
-    this.setState({ stars: response.data });
+    this.setState({
+      stars: response.data,
+      loading: false,
+    });
     // console.log(this.state.stars);
   }
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { stars, loading } = this.state;
 
     const user = navigation.getParam('user');
 
@@ -60,19 +68,25 @@ export default class User extends Component {
           <Name> {user.name} </Name>
           <Bio> {user.bio} </Bio>
         </Header>
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title> {item.name} </Title>
-                <Author> {item.owner.login} </Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+            onEndReached={this.loadMore} // Função que carrega mais itens
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title> {item.name} </Title>
+                  <Author> {item.owner.login} </Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
